aafa8dbc1a3152b52cd18cd5f382322e4473d80e
GokulGk <94182802+GKprojects@users.noreply.github.com>
2023-11-15 12:50:27 +0530

    Update SearchRepositoryTest.java

--- src/test/java/org/workshop/coffee/repository/SearchRepositoryTest.java
+++ src/test/java/org/workshop/coffee/repository/SearchRepositoryTest.java
@@ -1,64 +1,54 @@
-package org.workshop.coffee.repository;
-
-import org.junit.jupiter.api.Test;
-import org.workshop.coffee.domain.Product;
-
 import static org.junit.jupiter.api.Assertions.*;
-import java.util.List;
 
-public class SearchRepositoryTest {
-    
+class SearchRepositoryTest {
+
     @Test
-    public void testSearchProduct() {
+    void testSearchProduct() {
         SearchRepository searchRepository = new SearchRepository();
         List<Product> products = searchRepository.searchProduct("apple");
-        
-        assertNotNull("Products should not be null", products);
 
+        assertNotNull(products);
         assertEquals(2, products.size());
+
         assertEquals("Apple iPhone 12", products.get(0).getProductName());
         assertEquals("The latest iPhone from Apple", products.get(0).getDescription());
         assertEquals("Apple MacBook Pro", products.get(1).getProductName());
         assertEquals("Powerful laptop from Apple", products.get(1).getDescription());
     }
-    
+
     @Test
-    public void testSearchProductWithEmptyInput() {
+    void testSearchProductWithEmptyInput() {
         SearchRepository searchRepository = new SearchRepository();
         List<Product> products = searchRepository.searchProduct("");
-        // assertNotNull(products);
-        assertNotNull("Products should not be null", products);
 
+        assertNotNull(products);
         assertEquals(0, products.size());
-        assertEquals("Expected 0 products for null input", 0, products.size());
-
     }
-    
+
     @Test
-    public void testSearchProductWithNullInput() {
+    void testSearchProductWithNullInput() {
         SearchRepository searchRepository = new SearchRepository();
         List<Product> products = searchRepository.searchProduct(null);
-        // assertNotNull(products);
-        assertNotNull("Products should not be null", products);
 
+        assertNotNull(products);
         assertEquals(0, products.size());
     }
-    
+
     @Test
-    public void testSearchProductWithSQLInjection() {
+    void testSearchProductWithSQLInjection() {
         SearchRepository searchRepository = new SearchRepository();
         List<Product> products = searchRepository.searchProduct("'; DROP TABLE Product; --");
+
         assertNotNull(products);
-        assertNotNull("Products should not be null", products);
         assertEquals(0, products.size());
     }
-    
+
     @Test
-    public void testSearchProductWithXSSAttack() {
+    void testSearchProductWithXSSAttack() {
         SearchRepository searchRepository = new SearchRepository();
         List<Product> products = searchRepository.searchProduct("<script>alert('XSS Attack');</script>");
+
         assertNotNull(products);
-        assertNotNull("Products should not be null", products);
         assertEquals(0, products.size());
     }
 }
